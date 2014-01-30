de1bfc9ffdecd68af3f34cd8791b9d340af65d92
bcaccinolo <benoit.caccinolo@gmail.com>
2014-01-30 11:03:16 +0100

    some update

--- move.py
+++ move.py
@@ -1,3 +1,5 @@
+import re
+
 class Move(object):
 
     def __init__(self):
@@ -9,6 +11,17 @@ class Move(object):
 
     def dump(self):
         return '{}. {} {}'.format(self.id, self.white, self.black)
+
+    def validate_format(self, str_move):
+        return re.match('^\d+\.+\s*.*', str_move)
+
+    def parse(self, str_move):
+        if self.validate_format(str_move):
+            return "ERROR" # should raise an error here
+
+
+
+
 
 
 
--- test_move.py
+++ test_move.py
@@ -13,3 +13,17 @@ class TestMove(unittest.TestCase):
         m.white = 'e4'
         m.black = 'd5'
         self.assertEqual(m.dump(), '1. e4 d5')
+
+    def test_validate_format(self):
+        m = Move()
+        moveOK = '1. e4 { comment }'
+        moveKO = 'a. 11 '
+        self.assertNotEqual(m.validate_format(moveOK), None)
+        self.assertEqual(m.validate_format(moveKO), None)
+
+    def test_parse(self):
+        move1 = '1. e4 { comment }'
+        m = Move()
+        m.parse(move1)
+        self.assertEqual(m.dump(), move1)
+
